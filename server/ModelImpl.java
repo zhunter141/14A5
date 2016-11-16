9bcf22c6d692084c5fba3174475f29fded3f670c
Victor Fuentes Sangabriel <vfuentes1213@gmail.com>
2016-11-15 18:50:51 -0700

    vf Implementing adding players to game being updated on boards.

--- server/ModelImpl.java
+++ server/ModelImpl.java
@@ -6,12 +6,21 @@ import java.util.ArrayList;
 
 import common.ModelInterface;
 import common.ViewInterface;
+import cs414.a5.Bank;
 import cs414.a5.Board;
+import cs414.a5.Dice;
+import cs414.a5.Player;
+import cs414.a5.Token;
+import cs414.a5.View;
 
 @SuppressWarnings("serial")
 public class ModelImpl extends UnicastRemoteObject implements ModelInterface{
 	private ArrayList<ViewInterface> observers = new ArrayList<ViewInterface>();
 	private Board board;
+	private Player[] players;
+	private Token[] allTokens;
+	private Bank monopolyBank;
+	private int counter;
 	
 	public ModelImpl() throws RemoteException {
 		super();	
@@ -45,5 +54,10 @@ public class ModelImpl extends UnicastRemoteObject implements ModelInterface{
 	@Override
 	public void addPlayer(String playerName) throws RemoteException{
 		System.out.println("Adding "+playerName+" to game!");
+		Player p = new Player(counter,name,allTokens[counter]);
+		allTokens[counter].setLoc(board.getStart());
+		players[counter] = p;
+		counter++;
+		monopolyBank.addClient(p);
 	}
 }
