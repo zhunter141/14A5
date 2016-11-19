d6054a8e70fef303827accb9ab1857b9cabbf0d1
zhunter141 <zhunter141@gmail.com>
2016-11-18 17:57:15 -0700

    Fixed get out of jail free card to work with both go to jail card and square.

--- cs414/a5/DeckOfCards.java
+++ cs414/a5/DeckOfCards.java
@@ -18,18 +18,18 @@ public class DeckOfCards implements Serializable {
 			cards.add(card[x]);
 		}
 		
-		Collections.shuffle(cards);
+		//Collections.shuffle(cards);
 	}
 
 	// Should cover both draw the top card and return it back in the bottom
 	public Card drawCard() {
 		firstCard = cards.getFirst();
 		cards.removeFirst();
-		if(firstCard.getDescription().equals("Get out of Jail free")){
-			//do nothing 
-		}
-		else{cards.addLast(firstCard);}
-		
+		//if(firstCard.getDescription().equals("Get out of Jail free")){
+		//	//do nothing 
+		//}
+		//else{cards.addLast(firstCard);}
+		cards.addLast(firstCard);
 		return firstCard;
 	}
 	
